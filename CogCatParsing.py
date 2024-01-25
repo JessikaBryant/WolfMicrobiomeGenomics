83ca922e5485ef377c42f5225cbc26edf81a9d4a
Jessika Bryant <bryantj2@oregonstate.edu>
2024-01-24 17:20:41 -0800

    Added new python script

---
+++ CogCatParsing.py
@@ -0,0 +1,38 @@
+###load any needed modules
+
+
+###set working directory
+
+
+###Get path to folder with TSV files and store as variable
+
+
+###create a list of filenames
+#module glob.glob
+#give variable and pattern (such as *.tsv) and gives list of path to files
+
+
+
+###create dataframe with filename as first column
+#Create COG Category as empty columns (title)
+
+###forloop
+#for name in file read in tsv
+#insert previous forloop for each 
+
+
+
+
+
+
+
+
+
+
+
+
+
+###turn dicts/df back into TSV
+
+
+
