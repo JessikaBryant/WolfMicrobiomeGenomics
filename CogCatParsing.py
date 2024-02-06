08b48bfa40cec9baf22bf877a89c6c54501cf6ea
Jessika Bryant <bryantj2@oregonstate.edu>
2024-02-06 13:18:54 -0800

    Updated CogCatParsing.py file to most recent

--- CogCatParsing.py
+++ CogCatParsing.py
@@ -12,51 +12,59 @@ os.chdir('//Users/jessikagrindstaff/Documents/Documents - Jessika’s MacBook Ai
 ###create a list of filenames and a list of COG category letters
 #module glob.glob
 #give variable and pattern (such as *.tsv) and gives list of path to files
-tsvfiles=glob.glob('*.tsv')
-
 CatList=list('A''B''C''D''E''F''G''H''I''J''K''L''M''N''O''P''Q''R''S''T''U''V''W''X''Z')
 
-###create dataframe with filename as first column
-#Create COG Category as empty columns (title)
+
 tsvfiles=glob.glob('*.tsv')
 
-twowaytabledf=pd.DataFrame(tsvfiles)
+twowaytabledf=pd.DataFrame()
+
+twowaytabledf.index=tsvfiles
+
 
 #add Cat letter to dataframe
 for letter in CatList:
     twowaytabledf[letter]= np.nan
-
+     
 #replace NaN with 0 
-twowaytabledf.replace([np.nan, -np.inf], 0)
+twowaytabledf.replace([np.nan, -np.inf], int(0))
 
-###forloop to make one dataframe containing species names, cog category letter 
-###(will update to category meaning) and number of counts for each letter
 
-##below is a work in progress not finished
 for file in tsvfiles:
     #create a temporary dataframe to store matches
-    tempemptydf=pd.DataFrame(tsvfiles)
+    #tempemptydf=pd.DataFrame(tsvfiles)
+    
     #read in each tsvfile as a temporary dataframe to easily read
     tempfulldf=pd.read_csv(str(file), sep="\t")
+    
+    #Get the "Cat" column as a list
+    cats_list_temp=list(tempfulldf['Cat'])
+        
+    cleanedList = [x for x in cats_list_temp if x == x]
+    
     #remove the NaN from each dataframe
-    newtemfullpdf=tempfulldf.replace([np.nan, -np.inf], 0)
+    #newtemfullpdf=tempfulldf.replace([np.nan, -np.inf], 0)
+    
     #store the Cat column to lists
-    tempcatlists=list(newtemfullpdf["Cat"])
+    #tempcatlists=list(newtemfullpdf["Cat"])
+    
     #loop through categories
     for letter in CatList:
         #add Cat letter to temporary dataframe
-        tempemptydf[letter]=np.nan
+        #tempemptydf[letter]=np.nan
         #remove the NaN and replace as 0
-        tempemptydf.replace([np.nan, -np.inf], 0)
+        #tempemptydf.replace([np.nan, -np.inf], 0)
         #Use the findall function to ‘count’ the number of occurances of the letter in the list
-        matches=re.findall(letter, str(tempcatlists))
+        print(letter)
+        matches=re.findall(letter, str(cleanedList))
         #Add the number of occurances to the temporary "empty" dataframe
-        tempemptydf[letter]=len(matches)
-        print(tempemptydf)
-        #twowaytabledf[letter]=tempemptydf
+        #tempemptydf[letter]=len(matches)
+        print(len(matches))
+        
+        #add values to 
+        twowaytabledf.loc[file,letter]=int(len(matches))
 
+print(twowaytabledf)
 
-###turn dicts/df back into TSV
-
-
+twowaytabledf.to_csv("twowaytabledf.tsv",sep="\t")
 
